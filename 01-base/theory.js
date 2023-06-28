6b70ce0e7af269d736ee9888644ad6405bfcccb4
Ruslan Gumirov <gumirus@yandex.ru>
2023-06-28 17:05:46 +0500

    base

---
+++ 01-base/theory.js
@@ -0,0 +1,31 @@
+// Operators
+// c = c + a;
+// c = c - a;
+// c = c * a;
+// c = c / a;
+
+// c += a;
+// c -= a;
+// c *= a;
+// c /= a;
+
+// Data Types
+const age = 40 // number
+const name = "Agori" // string
+const IsProgrammer = true // boolean
+let x // undefined
+// null
+// console.log(typeof(1 / x))
+// console.log(typeof NaN)
+
+// Priority
+const fullAge = 40;
+const birthYear = 1982;
+const currentYaer = 2023;
+// > < >= <= 
+const isFullAge = currentYaer - birthYear >= fullAge;
+// console.log(isFullAge);
+
+const num1 = 34
+const num2 = "34"
+// console.log(num1 === num2);
