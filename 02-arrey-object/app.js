45317da11a7550582afd99945ccefcd32facd56c
Ruslan Gumirov <gumirus@yandex.ru>
2023-07-05 15:03:49 +0500

    object end

--- 02-arrey-object/app.js
+++ 02-arrey-object/app.js
@@ -19,15 +19,85 @@ const listElement = document.getElementById('list');
 
 // console.log(inputElement.value)
 
-const notes = ['записать блок про массивы', 'рассказать теорию объектов'];
+// const notes = ['записать блок про массивы', 'рассказать теорию объектов'];
 
-function render() { 
-  // for (let i = 0; i < notes.length; i++) {
-  //   listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]));
-  // }
-  for (let note of notes) {
-    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note));
+// function render() {
+// for (let i = 0; i < notes.length; i++) {
+//   listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]));
+// }
+//   for (let note of notes) {
+//     listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note));
+//   }
+// }
+
+// render();
+
+// createBtn.onclick = function () {
+//   if (inputElement.value.length === 0) {
+//     return;
+//   }
+
+//   listElement.insertAdjacentHTML(
+//     'beforeend',
+//     getNoteTemplate(inputElement.value)
+//   );
+//   inputElement.value = '';
+
+// };
+
+// function getNoteTemplate(title) {
+//     return `
+//     <li class="list-group-item d-flex justify-content-between align-items-center"
+//   >
+//    <span>${title}</span>
+//    <span>
+//      <span class="btn btn-small btn-success">&check;</span>
+//      <span class="btn btn-small btn-danger">&times;</span>
+//    </span>
+//   </li>`;
+// }
+
+/**
+ * Object Theory
+ 
+const person = {
+  firstName: 'agori',
+  lastName: 'andras',
+  year: 1982,
+  hasGirlfrriend: true,
+  languages: ['ru', 'gr'],
+  getFullName: function () {
+    console.log(person.firstName)
   }
+}
+
+// console.log(typeof notes)
+
+const key = 'hasGirlfrriend'
+*/
+
+const notes = [
+  {
+    title: 'записать блок про массивы',
+    completed: false,
+  },
+  {
+    title: 'рассказать теорию объектов',
+    completed: true,
+  },
+];
+
+function render() {
+  listElement.innerHTML = '';
+  if (notes.length === 0) {
+    listElement.innerHTML = '<p>Удалены все заметки</p>';
+  }
+  for (let i = 0; i < notes.length; i++) {
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i));
+  }
+  //   for (let note of notes) {
+  //     listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note));
+  //   }
 }
 
 render();
@@ -37,22 +107,50 @@ createBtn.onclick = function () {
     return;
   }
 
-  listElement.insertAdjacentHTML(
-    'beforeend',
-    getNoteTemplate(inputElement.value)
-  );
+  const newNote = {
+    title: inputElement.value,
+    completed: false,
+  };
+
+  notes.push(newNote);
+  render();
+
+  // listElement.insertAdjacentHTML(
+  //   'beforeend',
+  //   getNoteTemplate(newNote)
+  // );
   inputElement.value = '';
-  
 };
 
-function getNoteTemplate(title) {
-    return `
+listElement.onclick = function (event) {
+  if (event.target.dataset.index) {
+    // const index = Number(event.target.dataset.index)
+    const index = parseInt(event.target.dataset.index);
+    const type = event.target.dataset.type;
+
+    if (type === 'toggle') {
+      notes[index].completed = !notes[index].completed;
+    } else if (type === 'remove') {
+      notes.splice(index, 1);
+    }
+    render();
+  }
+};
+
+function getNoteTemplate(note, index) {
+  return `
     <li class="list-group-item d-flex justify-content-between align-items-center"
   >
-   <span>${title}</span>
+   <span class="${note.completed ? 'text-decoration-line-through' : ''}">${
+    note.title
+  }</span>
    <span>
-     <span class="btn btn-small btn-success">&check;</span>
-     <span class="btn btn-small btn-danger">&times;</span>
+     <span class="btn btn-small btn-${
+       note.completed ? 'warning' : 'success'
+     }"data-index="${index}" data-type="toggle">&check;</span>
+     <span class="btn btn-small btn-danger"data-type="remove"
+     data-index="${index}">&times;</span>
    </span>
-  </li>`;
-  }
+  </li>
+  `;
+}
